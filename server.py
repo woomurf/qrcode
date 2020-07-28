afc177bab95cd54bd46c384ec75a4971d1c4cc71
woomurf <dngusdnd@gmail.com>
2020-07-28 18:27:41 +0900

    threaded False

--- server.py
+++ server.py
@@ -41,4 +41,4 @@ def healthCheck():
     return "ok", 200
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80, debug=True)
+    app.run(host='0.0.0.0', port=80, debug=True, threaded=False)
