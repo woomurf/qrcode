fc27ef62d1923b520bec7e6a6023a2764c087e0f
woomurf <dngusdnd@gmail.com>
2020-07-29 12:50:42 +0900

    handling queue

--- server.py
+++ server.py
@@ -1,9 +1,34 @@
-from flask import Flask, request, send_file, render_template
+from flask import Flask, request, send_file, render_template, jsonify
 from MyQR import myqr
 from PIL import Image
 import os
+import time 
+import threading
+from queue import Queue, Empty
 
 app = Flask(__name__, template_folder='./templates/')
+
+requests_queue = Queue()
+BATCH_SIZE = 1
+REQUEST_LIMIT = 5
+CHECK_INTERVAL = 0.1
+
+def handle_requests_by_batch():
+    while True:
+        requests_batch = []
+        while not (len(requests_batch) >= BATCH_SIZE):
+            try:
+                requests_batch.append(requests_queue.get(timeout=CHECK_INTERVAL))
+            except Empty:
+                continue
+            batch_outputs = []
+            for request in requests_batch:
+                batch_outputs.append(run(request['input'][0], request['input'][1]))
+            
+            for request, output in zip(requests_batch, batch_outputs):
+                request['output'] = output
+
+threading.Thread(target=handle_requests_by_batch).start()
 
 @app.route("/", methods=["GET"])
 def index():
@@ -11,6 +36,10 @@ def index():
 
 @app.route("/qr", methods=["POST"])
 def generateQRcode():
+
+    if requests_queue.qsize() > REQUEST_LIMIT: 
+        return jsonify({'error': 'Too Many Requests'}), 429
+
     url = request.form['url']
     if 'image' in request.files: 
         image = Image.open(request.files['image'].stream)
@@ -18,8 +47,25 @@ def generateQRcode():
     else:
         image = None
         format_ = "PNG"
+    
+    req = {
+        'input': [url, image]
+    }
 
+    requests_queue.put(req)
 
+    while 'output' not in req:
+        time.sleep(CHECK_INTERVAL)
+    
+    qr = req['output']
+    
+    return send_file(qr, mimetype='image/'+format_)
+
+@app.route("/healthz", methods=["GET"])
+def healthCheck():
+    return "ok", 200
+
+def run(url, image):
     version, level, qr = myqr.run(
         url,
         version=1,
@@ -34,11 +80,7 @@ def generateQRcode():
 
     qr.seek(0)
     
-    return send_file(qr, mimetype='image/'+format_)
-
-@app.route("/healthz", methods=["GET"])
-def healthCheck():
-    return "ok", 200
+    return qr
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80, debug=True, threaded=False)
+    app.run(host='0.0.0.0', port=80)
